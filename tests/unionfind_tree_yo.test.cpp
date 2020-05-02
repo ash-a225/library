c3f0af8421d4f8a2284914fe5499f24d7cd62802
ash-a225 <abekawa.a.aa@m.titech.ac.jp>
2020-05-03 01:47:25 +0900

    add unionfind_test

---
+++ tests/unionfind_tree_yo.test.cpp
@@ -0,0 +1,28 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/unionfind"
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define all(x) (x).begin(),(x).end()
+using namespace std;
+using ll = long long;
+using P = pair<int,int>;
+
+#include "DataStructure/unionfind.cpp"
+
+int main() {
+  std::cin.tie(nullptr);
+  std::ios_base::sync_with_stdio(false);
+  std::cout << std::fixed << std::setprecision(15);
+  int n, q;
+  cin >> n >> q;
+  UnionFind tree(n);
+  rep(i,q) {
+    int t, u, v;
+    cin >> t >> u >> v;
+    if (t == 0) tree.merge(u, v);
+    else {
+      if (tree.same(u,v)) cout << 1 << "\n";
+      else cout << 0 << "\n";
+    }
+  }
+  return 0;
+}

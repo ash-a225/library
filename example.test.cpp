93de175e6d263fc845a708d534d243814701d496
ash-a225 <abekawa.a.aa@m.titech.ac.jp>
2020-05-03 00:47:51 +0900

    test

--- example.test.cpp
+++ example.test.cpp
@@ -1,23 +1,28 @@
 #define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/4/ALDS1_4_B"
-#include <algorithm>
-#include <iostream>
-#include <vector>
-#define REP(i, n) for (int i = 0; (i) < (int)(n); ++ (i))
-#define ALL(x) std::begin(x), std::end(x)
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define all(x) (x).begin(),(x).end()
 using namespace std;
+using ll = long long;
+using P = pair<int,int>;
+template <class T> void chmin(T &a, const T &b) noexcept { if (b < a) a = b; }
+template <class T> void chmax(T &a, const T &b) noexcept { if (a < b) a = b; }
 
 int main() {
+  std::cin.tie(nullptr);
+  std::ios_base::sync_with_stdio(false);
+  std::cout << std::fixed << std::setprecision(15);
     int n; cin >> n;
     vector<int> s(n);
-    REP (i, n) {
+    rep(i, n) {
         cin >> s[i];
     }
     int q; cin >> q;
     int cnt = 0;
     while (q --) {
         int t_i; cin >> t_i;
-        cnt += binary_search(ALL(s), t_i);
+        cnt += binary_search(all(s), t_i);
     }
     cout << cnt << endl;
-    return 0;
+  return 0;
 }

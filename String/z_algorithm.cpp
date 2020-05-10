ddae5ee2687bc2616a8476074a6890bc2fa05368
ash-a225 <abekawa.a.aa@m.titech.ac.jp>
2020-05-10 15:54:52 +0900

    add z-algorithm

---
+++ String/z_algorithm.cpp
@@ -0,0 +1,25 @@
+#ifndef Z_ALGO_H
+#define Z_ALGO_H
+
+struct z_array {
+  vector<int> prefix;
+  void build(const string &s) {
+    prefix.assign(s.size(),0);
+    for(int i=1, j=0; i < (int)s.size(); ++i) {
+      if(i+prefix[i-j] < j+prefix[j]) {
+        prefix[i] = prefix[i-j];
+      }
+      else {
+        int k = std::max(0,j+prefix[j]-i);
+        while(i+k < (int)s.size() && s[k] == s[i+k]) ++k;
+        prefix[i] = k;
+        j = i;
+      }
+    }
+    prefix[0] = s.size();
+  }
+  int operator[](const int k) const{ return prefix[k];}
+  int size(){ return prefix.size();}
+};
+
+#endif
--- tests/yj_z_algorithm.test.cpp
+++ tests/yj_z_algorithm.test.cpp
@@ -1,4 +1,3 @@
-#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/4/ALDS1_4_B"
 #include <bits/stdc++.h>
 #define rep(i,n) for (int i = 0; i < (n); ++i)
 #define all(x) (x).begin(),(x).end()
@@ -8,21 +7,19 @@ using P = pair<int,int>;
 template <class T> void chmin(T &a, const T &b) noexcept { if (b < a) a = b; }
 template <class T> void chmax(T &a, const T &b) noexcept { if (a < b) a = b; }
 
+#include "String/z_algorithm.cpp"
+
 int main() {
   std::cin.tie(nullptr);
   std::ios_base::sync_with_stdio(false);
   std::cout << std::fixed << std::setprecision(15);
-    int n; cin >> n;
-    vector<int> s(n);
-    rep(i, n) {
-        cin >> s[i];
-    }
-    int q; cin >> q;
-    int cnt = 0;
-    while (q --) {
-        int t_i; cin >> t_i;
-        cnt += binary_search(all(s), t_i);
-    }
-    cout << cnt << endl;
+  string str;
+  cin >> str;
+  z_array a; //LCP
+  a.build(str);
+  rep(i,(int)str.size()) {
+    cout << a[i] << " ";
+  }
+  cout << endl;
   return 0;
 }

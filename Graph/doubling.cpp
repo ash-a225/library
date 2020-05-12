606c141093fe867b6c3e74ab5c8790420da5a518
ash-a225 <abekawa.a.aa@m.titech.ac.jp>
2020-05-12 20:08:03 +0900

    add sparce table, doubling

---
+++ Graph/doubling.cpp
@@ -0,0 +1,66 @@
+#ifndef DOUBLING_H
+#define DOUBLING_H
+
+//to[i][v] vの2^i個上の頂点
+
+template<typename T>
+struct Doubling {
+  private:
+    const vector<vector<int> > &G;
+    vector<vector<int> > to;
+    vector<vector<T> > sum_w, min_w;
+  public:
+    Doubling(const vector<pair<int,T> > &G):G(G){};
+    void build(ll max_k) {
+      int k = 1;
+      while ((1LL<<k) <= max_k) k++;
+      int n = G.size();
+      to.assign(k, vector<int>(n,-1));
+      sum_w.assign(k, vector<T>(n,0));
+      min_w.assign(k, vector<T>(n,numeric_limits<T>::max()));
+      for(int i=0; i<k-1; ++i) {
+        for(int v=0: v<n; ++v) {
+          if (to[i][v] < 0) to[i+1][v] = -1;
+          else {
+            to[i+1][v] = to[i][to[i][v]];
+            sum_w[i+1][v] = sum_w[i][v] + sum_w[i][to[i][v]];
+            min_w[i+1][v] = min(min_w[i][v], min_w[i][to[i][v]]);
+          }
+        }
+      }
+    }
+    int get_i(int i, ll k) { //iのk個先
+      assert(i < (int)G.size());
+      int ind = i;
+      for (int j=0; j<k; ++j) {
+        if (k & (1LL<<j)) ind = to[j][ind];
+      }
+      return ind;
+    }
+    T get_sum_w(int i, ll k) {
+      assert(i < (int)G.size());
+      int ind = i;
+      ll sum = 0;
+      for (int j=0; j<k; ++j) {
+        if (k & (1LL<<j)) {
+          sum += sum_w[j][ind];
+          ind = to[j][ind];
+        }
+      }
+      return sum;
+    }
+    T get_min_w(int i, ll k) {
+      assert(i < (int)G.size());
+      int ind = i;
+      ll mini = 0;
+      for (int j=0; j<k; ++j) {
+        if (k & (1LL<<j)) {
+          chmin(mini, min_w[j][ind]);
+          ind = to[j][ind];
+        }
+      }
+      return mini;
+    }
+};
+
+#endif
---
+++ tests/yj_static_rmq2.test.cpp
@@ -0,0 +1,28 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/point_add_range_sum"
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define all(x) (x).begin(),(x).end()
+using namespace std;
+using ll = long long;
+using P = pair<int,ll>;
+template <class T> void chmin(T &a, const T &b) noexcept { if (b < a) a = b; }
+template <class T> void chmax(T &a, const T &b) noexcept { if (a < b) a = b; }
+
+#include "DataStructure/sparce_table.cpp"
+
+int main() {
+  std::cin.tie(nullptr);
+  std::ios_base::sync_with_stdio(false);
+  std::cout << std::fixed << std::setprecision(15);
+  int n, q;
+  cin >> n >> q;
+  vector<ll> a(n);
+  rep(i,n) cin >> a[i];
+  SparseTable<ll> st(a);
+  rep(_,q) {
+    int l, r;
+    cin >> l >> r;
+    cout << st.query(l,r) << "\n";
+  }
+  return 0;
+}

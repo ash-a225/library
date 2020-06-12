2cb52142dccf30a9b422f85a5bc2ae4b3e5c87b1
ash-a225 <abekawa.a.aa@m.titech.ac.jp>
2020-06-12 17:25:35 +0900

    add ntt tests

---
+++ tests/yj_convolution1000000007.test.cpp
@@ -0,0 +1,28 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/convolution_mod_1000000007"
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define all(x) (x).begin(),(x).end()
+using namespace std;
+using ll = long long;
+using P = pair<int,int>;
+template <class T> void chmin(T &a, const T &b) noexcept { if (b < a) a = b; }
+template <class T> void chmax(T &a, const T &b) noexcept { if (a < b) a = b; }
+
+#include "Math/ntt.cpp"
+
+int main() {
+  std::cin.tie(nullptr);
+  std::ios_base::sync_with_stdio(false);
+  std::cout << std::fixed << std::setprecision(15);
+  int n, m;
+  cin >> n >> m;
+  vector<ll> a(n), b(m);
+  rep(i,n) cin >> a[i];
+  rep(i,m) cin >> b[i];
+
+  auto ans = ntt_convolve(a, b, 1000000007);
+  rep(i,n+m-1) {
+    cout << ans[i] << "\n";
+  }
+  return 0;
+}

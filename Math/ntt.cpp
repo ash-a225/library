9a31f56b7fb7a61baab143f2d896ab775531d3ed
ash-a225 <abekawa.a.aa@m.titech.ac.jp>
2020-06-12 15:14:28 +0900

    ntt 高速化

--- Math/ntt.cpp
+++ Math/ntt.cpp
@@ -1,33 +1,9 @@
-#include <bits/stdc++.h>
-#define rep(i,n) for (int i = 0; i < (n); ++i)
-#define all(x) (x).begin(),(x).end()
-using namespace std;
-using ll = long long;
-using P = pair<int,int>;
-template <class T> void chmin(T &a, const T &b) noexcept { if (b < a) a = b; }
-template <class T> void chmax(T &a, const T &b) noexcept { if (a < b) a = b; }
-void debug_out() { cout << "\n"; }
-template <class T, class... Args>
-void debug_out(const T &x, const Args &... args) { cout << x << " "; debug_out(args...);}
-#ifdef _DEBUG
-  #define debug(...) debug_out(__VA_ARGS__)
-#else
-  #define debug(...) 
-#endif
-
-int main() {
-  std::cin.tie(nullptr);
-  std::ios_base::sync_with_stdio(false);
-  std::cout << std::fixed << std::setprecision(15);
-  return 0;
-}
-
 #include "Math/modint.cpp"
 
-template<typename ModInt>
-struct NTT {
+template<typename ModInt> struct NTT {
 private:
   using mi = ModInt;
+
   static mi primitive_root() {
     ll p = mi{}.get_mod();
     if (p == 167772161) return 3;
@@ -42,6 +18,7 @@ private:
     x = (x << 24) | ((x & 0xFF00) << 8) | ((x >> 8) & 0xFF00) | (x >> 24);
     return x;
   }
+
 public:
   void ntt(vector<mi> &A, const int k, bool inverse = false) {
     if (k == 0) return;
@@ -52,14 +29,16 @@ public:
       }  
     }
     for (int i = 1; i <= k; ++i) { //N = (1<<i)
+      mi w = primitive_root().pow((mi(-1)/(1<<i)).x);
+      if (inverse) w = mi(1) / w;
       for (int j = 0; j < (1<<(k-i)); ++j) { 
+        mi base = 1;
         for (int l = 0; l < (1<<(i-1)); ++l) { //l = 0,1...N/2-1
-          mi w = primitive_root().pow((mi(-l)/(1<<i)).x);
-          if (inverse) w = mi(1) / w;
           mi s = A[(1<<i)*j + l];
-          mi t = A[(1<<i)*j + l + (1<<(i-1))] * w; //polar(1.0, (inverse ? -1 : 1)*2.0*PI*l/(1<<i));
+          mi t = A[(1<<i)*j + l + (1<<(i-1))] * base;
           A[(1<<i)*j + l] = s + t;
           A[(1<<i)*j + l + (1<<(i-1))] = s - t;
+          base *= w;
         }
       }
     }
